64df534564638cfadd8e579335eb88854123b71a
AlbertoDam <alberto97.dam@gmail.com>
2022-11-14 20:14:47 +0000

    Autopush

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd Ejercicios/Eje1
+mkdir DAM
+cd DAM
+mkdir primero
+mkdir segundo
+cd primero
+mkdir PRO SSF LND LNT FOL ETS BAE
+cd ..
+cd segundo
+mkdir PGV PGL EMR FCT AED DAD S8G PPP
+
